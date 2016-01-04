1d4a064e77d15cab92de24bdbea31c5ad13ae339
Matthieu Freeman <me@ilambda.io>
2016-01-04 06:15:12 +0100

    Added test and fix in module

--- test/mainframe-module-auth.js
+++ test/mainframe-module-auth.js
@@ -1,3 +1,20 @@
 // requiring modules
 import Mainframe from 'mainframe-core'
 import AuthModule from '../lib/mainframe-module-auth.js'
+import * as _ from 'lodash'
+
+// create the mainframes
+let mainframe = new Mainframe(new AuthModule())
+
+// register users
+mainframe.auth.register({ name:'picard', password:'paris' }, (err, res) => {
+  // connect user
+  mainframe.auth.login({ name: 'picard', password: 'paris' }, (err, res) => {
+    // list users
+    console.log(mainframe.auth.users, _.keys(mainframe.brain.layers))
+    // log out
+    mainframe.auth.logout('picard')
+    // list users
+    console.log(mainframe.auth.users, _.keys(mainframe.brain.layers))
+  })
+})
